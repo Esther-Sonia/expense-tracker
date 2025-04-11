efcfbf44bce09385c062fb4afb8e30377cdf9265
Esther-Sonia <esthersonia21@gmail.com>
2025-04-11 23:52:05 +0300

    build core App structure and connect child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'; 
+import ExpenseForm from "./assets/components/ExpenseForm";
+import ExpenseTable from "./assets/components/ExpenseTable";
+import SearchBar from "./assets/components/Searchbar";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const addExpense = (newExpense) => {
+    setExpenses([...expenses, newExpense]);
+  };
+
+  const filteredExpenses = expenses.filter((expense) =>
+    expense.expense.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    expense.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return(
+    <div style={{ display: "flex", padding: "20px", fontFamily: "Arial" }}>
+        <div style={{ width: "25%", marginRight: "20px" }}>
+  <h2>Add Expense</h2>
+    <ExpenseForm toAddExpense={addExpense} />
+  </div>
+
+  <div style={{ width: "75%" }}>
+  <h1>Expense Tracker</h1>
+  <SearchBar setSearchTerm={setSearchTerm} />
+  <ExpenseTable expenses={filteredExpenses} />
+</div>
+</div>
+
+  );
 }
 
-export default App
+export default App;
+
+
