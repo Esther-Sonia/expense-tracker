ff1364057890b5458e1421933cfcd831b883a85a
Esther-Sonia <esthersonia21@gmail.com>
2025-04-12 19:08:59 +0300

    add basic styling to ExpenseTable

--- src/assets/components/ExpenseTable.jsx
+++ src/assets/components/ExpenseTable.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 function ExpenseTable({ expenses }) {
     return (
-        <table>
-            <thead>
+        <table style={{ width: "100%", borderCollapse: "collapse" }}> 
+            <thead style={{ background: "#338AE7	", color: "#fff" }}>
               <tr>
-            <th>Expense</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Amount</th>
-            <th>Date</th>
+            <th style={thStyle}>Expense</th>
+            <th style={thStyle}>Description</th>
+            <th style={thStyle}>Category</th>
+            <th style={thStyle}>Amount</th>
+            <th style={thStyle}>Date</th>
             </tr>
             </thead>
             <tbody>
                 {expenses.map((exp, index) => 
                     <tr key={index}>
-                    <td>{exp.expense}</td>
-                    <td>{exp.description}</td>
-                    <td>{exp.category}</td>
-                    <td>{exp.amount}</td>
-                    <td>{exp.date}</td>
+                    <td style={tdStyle}>{exp.expense}</td>
+                    <td style={tdStyle}>{exp.description}</td>
+                    <td style={tdStyle}>{exp.category}</td>
+                    <td style={tdStyle}>{exp.amount}</td>
+                    <td style={tdStyle}>{exp.date}</td>
                     </tr>
                 )}
 
@@ -28,6 +28,17 @@ function ExpenseTable({ expenses }) {
         </table>
     );
 }
+
+const thStyle = {
+  padding: "10px",
+  textAlign: "left",
+};
+
+const tdStyle = {
+  padding: "10px",
+  textAlign: "left",
+};
+
 
 
 export default ExpenseTable;
