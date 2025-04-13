2647ba7fadb008b015c8a7754552235be572b460
Esther-Sonia <esthersonia21@gmail.com>
2025-04-13 19:41:13 +0300

    implement sort and delete functionality to expense table

--- src/assets/components/ExpenseTable.jsx
+++ src/assets/components/ExpenseTable.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ExpenseTable({ expenses }) {
+
+function ExpenseTable({ expenses, onDelete }) {
+    const [sortBy, setSortBy] = useState(null);
+
+
+    const sortedExpenses = [...expenses].sort((a, b) => {
+        if (!sortBy) return 0;
+    return a[sortBy].localeCompare(b[sortBy]);
+  });
+
+  const sortButtonStyle = {
+    marginLeft: "8px",
+    padding: "4px 8px",
+    fontSize: "12px",
+    backgroundColor: "#f0f0f0",
+  };
+
     return (
         <table style={{ width: "100%", minWidth: "800px", borderCollapse: "collapse" }}> 
             <thead style={{ background: "#338AE7	", color: "#fff" }}>
               <tr>
-            <th style={thStyle}>Expense</th>
-            <th style={thStyle}>Description</th>
-            <th style={thStyle}>Category</th>
+            <th style={thStyle} onClick={() => setSortBy('expense')} >Expense</th>
+            <th style={thStyle}> Description <button onClick={() => setSortBy('description')}style={sortButtonStyle} > Sort (A-Z)</button> </th>
+            <th style={thStyle}> Category <button onClick={() => setSortBy('category')}style={sortButtonStyle} > Sort (A-Z)</button> </th>
             <th style={thStyle}>Amount</th>
             <th style={thStyle}>Date</th>
+            <th style={thStyle}>Action</th> 
             </tr>
             </thead>
             <tbody>
-                {expenses.map((exp, index) => 
+                {sortedExpenses.map((exp, index) => 
                     <tr key={index}>
                     <td style={tdStyle}>{exp.expense}</td>
                     <td style={tdStyle}>{exp.description}</td>
                     <td style={tdStyle}>{exp.category}</td>
                     <td style={tdStyle}>{exp.amount}</td>
                     <td style={tdStyle}>{exp.date}</td>
+                    <td style={tdStyle}>
+              <button 
+                onClick={() => onDelete(index)}
+                style={{ 
+                  background: "#ff4444", 
+                  color: "white", 
+                  border: "none",
+                  padding: "10px",
+                  borderRadius: "6px"
+                }}
+              >
+                Delete
+              </button>
+            </td>
                     </tr>
                 )}
 
@@ -37,6 +68,8 @@ const thStyle = {
 const tdStyle = {
   padding: "10px",
   textAlign: "left",
+  cursor: "pointer",
+
 };
 
 
